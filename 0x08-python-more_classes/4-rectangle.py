824f9803a1a674ecbe1ee49c0f35d23ca9beba29
MoeFadul <fadulmoe@gmail.com>
2024-01-04 11:24:52 +0000

    4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""
+Defines  Rectangle class.
+"""
 class Rectangle:
+    """Represent the rectangle."""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
