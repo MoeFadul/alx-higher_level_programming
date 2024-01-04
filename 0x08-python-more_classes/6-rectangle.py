65990c97b40695f6b376da02dba41aa1c45be1cf
MoeFadul <fadulmoe@gmail.com>
2024-01-04 11:45:55 +0000

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+"""
+Defines  Rectangle class.
+"""
+
+
 class Rectangle:
+    """Represent the rectangle."""
+
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -32,23 +39,29 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return self.__width * self.__height
+        return (self.__width * self.__height)
 
     def perimeter(self):
-        return 2 * (self.__width + self.__height)
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
-            return ""
-        else:
-            rectangle_str = ""
-            for _ in range(self.__height):
-                rectangle_str += "#" * self.__width + "\n"
-            return rectangle_str.rstrip()
+            return ("")
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
 
     def __repr__(self):
-        return f"Rectangle({self.__width}, {self.__height})"
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
 
     def __del__(self):
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
