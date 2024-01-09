c551a1c570f6433bcb0d3fa026a61c98a00f3089
MoeFadul <fadulmoe@gmail.com>
2024-01-09 18:33:08 +0000

    mandatory

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,6 @@ Defines a text file-reading function
 """
 
 def read_file(filename=""):
+    """"prints whats in UTF8 to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
